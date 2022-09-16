efcfe050d556e8295b2442a250863121ed345f31
ykim0302 <y8kim@uwaterloo.ca>
2022-09-15 17:37:08 -0700

    Update Final1.py
    Added more comments

--- Final1.py
+++ Final1.py
@@ -5,9 +5,13 @@ import shutil
 
 # The location of the files that you want to sort
 source = 'C:/Users/kimye/Desktop/Coding/Python/Image_sorting_algorithm/123/'
+# The location where you want to copy them to
 destination = 'C:/Users/kimye/Desktop/Coding/Python/Image_sorting_algorithm/des/'
+
+# For photos taken on the same date, add different numbers for each
 count = 0 
 
+# Function that removes all other signs from dates
 def cleaner(filename):
     filename = filename.replace(':', '')
     filename = filename.replace('-', '')
@@ -18,6 +22,7 @@ def cleaner(filename):
 images = os.listdir(source)   
 
 for image in images : 
+# Get the created and modified time of the file and put them in a list
     oldname = source + image   
     created_time = str(datetime.date.fromtimestamp(os.path.getctime(source+image)))  
     modified_time = str(datetime.date.fromtimestamp(os.path.getmtime(source+image)))  
@@ -26,16 +31,18 @@ for image in images :
     timelist.append(cleaner(modified_time))
 
     try:
+# Get the meta data of the file
         photo = Image.open(source+image)
         meta = photo._getexif()
         taken_time = meta[36867] 
         timelist.append(cleaner(taken_time))
     except:  
+# If a meta data cannot be found, set it as the higest number so that it will be sorted as last in the timelist
         taken_time = 99999999
-        
+
+# The lowest(earliest) time will be selected and be renamed
     timelist.sort() 
     newname = destination + timelist[0] + " " + str(count)  + ".jpg"
-
     shutil.copy2(oldname, newname)   
     count += 1   
 
